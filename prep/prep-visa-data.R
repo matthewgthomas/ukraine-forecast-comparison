2d5aa521e6e7b7ad4f8607bf878236e8c4ac62f1
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2025-12-31 12:49:21 +0000

    Rename data columns and create train/test splits

--- prep/prep-visa-data.R
+++ prep/prep-visa-data.R
@@ -9,6 +9,8 @@ source(
   "https://github.com/britishredcrosssociety/ukraine-analyses/raw/main/R/load%20Ukraine%20visa%20data%20-%20Local%20Authorities.R"
 )
 
+# "week","visa_type","applications","visas_issued","arrivals"
+
 ## Make a dataframe containing weekly visa data ----
 # Containing weekly applications, visas issued, and arrivals for:
 # - Family Scheme (from scraped data)
@@ -30,13 +32,19 @@ weekly_family_scheme_visas <-
   ) |>
   # Calculate week-on-week changes
   mutate(
-    `Weekly applications` = `visa applications received` -
+    applications = `visa applications received` -
       lag(`visa applications received`),
-    `Weekly visas issued` = `visas issued` - lag(`visas issued`),
-    `Weekly arrivals` = `arrivals of visa-holders in the UK` -
+    visas_issued = `visas issued` - lag(`visas issued`),
+    arrivals = `arrivals of visa-holders in the UK` -
       lag(`arrivals of visa-holders in the UK`)
   ) |>
-  select(Week, Scheme, starts_with("Weekly")) |>
+  select(
+    week = Week,
+    visa_type = Scheme,
+    applications,
+    visas_issued,
+    arrivals
+  ) |>
   # Turn NAs into zeros
   mutate(across(where(is.numeric), \(x) replace_na(x, 0)))
 
@@ -45,17 +53,17 @@ weekly_sponsorship_scheme_visas <-
   arrange(Date) |>
   filter(str_detect(Type, "^Sponsored")) |>
   mutate(
-    Scheme = if_else(
+    visa_type = if_else(
       str_detect(Type, "Government"),
-      "Government 'super sponsored'",
+      "Government sponsored",
       Type
     )
   ) |>
   # Calculate UK weekly totals
   mutate(
-    Week = week(Date) + (52 * (year(Date) - 2022))
+    week = week(Date) + (52 * (year(Date) - 2022))
   ) |>
-  group_by(Week, Scheme) |>
+  group_by(week, visa_type) |>
   summarise(
     `Number of visa applications` = sum(`Number of visa applications`),
     `Number of visas issued` = sum(`Number of visas issued`),
@@ -65,62 +73,73 @@ weekly_sponsorship_scheme_visas <-
   ) |>
   ungroup() |>
   # Calculate week-on-week changes
-  group_by(Scheme) |>
+  group_by(visa_type) |>
   mutate(
-    `Weekly applications` = `Number of visa applications` -
+    applications = `Number of visa applications` -
       lag(`Number of visa applications`),
-    `Weekly visas issued` = `Number of visas issued` -
+    visas_issued = `Number of visas issued` -
       lag(`Number of visas issued`),
-    `Weekly arrivals` = `Number of arrivals` - lag(`Number of arrivals`)
+    arrivals = `Number of arrivals` - lag(`Number of arrivals`)
   ) |>
   ungroup() |>
-  select(Week, Scheme, starts_with("Weekly"))
+  select(week, visa_type, applications, visas_issued, arrivals)
 
 # Since March 2024, the LA-level data has been published fortnightly rather than weekly
 # This creates gaps when forecasting, so create a dataset with the full set of weeks using `expand_grid()`,
 # then impute the 'missing' weeks
 weekly_sponsorship_scheme_visas <-
   expand_grid(
-    Week = min(weekly_sponsorship_scheme_visas$Week):max(visas_scraped$Week),
-    Scheme = unique(weekly_sponsorship_scheme_visas$Scheme)
+    week = min(weekly_sponsorship_scheme_visas$week):max(visas_scraped$Week),
+    visa_type = unique(weekly_sponsorship_scheme_visas$visa_type)
   ) |>
   left_join(weekly_sponsorship_scheme_visas) |>
 
   # Impute missing weeks
-  group_by(Scheme) |>
+  group_by(visa_type) |>
   mutate(
-    `Weekly applications` = zoo::na.approx(
-      `Weekly applications`,
+    applications = zoo::na.approx(
+      applications,
       na.rm = FALSE
     ),
-    `Weekly visas issued` = zoo::na.approx(
-      `Weekly visas issued`,
+    visas_issued = zoo::na.approx(
+      visas_issued,
       na.rm = FALSE
     ),
-    `Weekly arrivals` = zoo::na.approx(`Weekly arrivals`, na.rm = FALSE)
+    arrivals = zoo::na.approx(arrivals, na.rm = FALSE)
   ) |>
 
   # Fill in any remaining NAs - which will be the latest weeks (and, so, couldn't have been imputed)
-  fill(starts_with("Weekly"), .direction = "down") |>
-
-  ungroup()
+  fill(applications:arrivals, .direction = "down") |>
+  ungroup() |>
+  select(week, visa_type, applications, visas_issued, arrivals)
 
 weekly_visas_by_scheme <- bind_rows(
   weekly_sponsorship_scheme_visas,
   weekly_family_scheme_visas
 ) |>
-  arrange(Week, Scheme) |>
+  arrange(week, visa_type) |>
   # Convert week number to date; source: https://stackoverflow.com/a/46183403
-  mutate(Date = ymd("2022-01-03") + weeks(Week - 1)) |>
-  relocate(Date) |>
+  mutate(date = ymd("2022-01-03") + weeks(week - 1)) |>
+  relocate(date) |>
+  select(-week)
 
-  # Replace NAs with zeros
-  mutate(
-    across(
-      c(`Weekly applications`, `Weekly visas issued`, `Weekly arrivals`),
-      ~ replace_na(.x, 0)
-    )
+# Replace NAs with zeros
+mutate(
+  across(
+    c(applications, visas_issued, arrivals),
+    ~ replace_na(.x, 0)
   )
+)
 
 # Save
 write_csv(weekly_visas_by_scheme, "data/visas-weekly.csv")
+
+# Split visa data into train and test sets, using 24th August 2022 as the cutoff
+# This is when I first developed and ran the forecasts
+weekly_visas_by_scheme |>
+  filter(date <= ymd("2022-08-24")) |>
+  write_csv("data/visas-weekly-training.csv")
+
+weekly_visas_by_scheme |>
+  filter(date > ymd("2022-08-24")) |>
+  write_csv("data/visas-weekly-test.csv")
